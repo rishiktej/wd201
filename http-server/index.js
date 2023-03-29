31e9d753816b821f531a226dc7a367c21be3ef80
rishiktej <123888788+rishiktej@users.noreply.github.com>
2023-03-29 14:59:22 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,61 +1,48 @@
 const http = require("http");
 const fs = require("fs");
-// const path = require("path");
-// const { argv } = require("process");
+const args=require("minimist")(process.argv.slice(2))
 
-// const portIndex = argv.indexOf("--port");
-// const port = portIndex !== -1 ? Number(argv[portIndex + 1]) : 3000;
-const arg=require("minimist")(process.argv.slice(2))
+let homeContent = "";
+let projectContent = "";
+let registrationcontent="";
 
-const server = http.createServer((req, res) => {
-  const { method, url: reqUrl } = req;
-
-  if (method === "GET" && reqUrl === "/registration") {
-    const filePath = path.join(__dirname, "registration.html");
-
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error loading file");
-        return;
-      }
-
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (method === "GET" && reqUrl === "/projects") {
-    const filePath = path.join(__dirname, "project.html");
-
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error loading file");
-        return;
-      }
-
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (method === "GET" && reqUrl === "/") {
-    const filePath = path.join(__dirname, "home.html");
-
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error loading file");
-        return;
-      }
-
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Page not found");
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
   }
+  homeContent = home;
 });
-server.listen(arg);
 
-// server.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
 });
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationcontent = registration;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationcontent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(args);
